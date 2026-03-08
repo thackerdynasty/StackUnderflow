726bb3a56f6b4792a83d16c249fde7aa9ad96031
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2026-03-08 14:36:39 -0500

    add database

--- StackUnderflow/Program.cs
+++ StackUnderflow/Program.cs
@@ -5,10 +5,10 @@ using StackUnderflow.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("ServerConnection") ??
+                       throw new InvalidOperationException("Connection string 'ServerConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
