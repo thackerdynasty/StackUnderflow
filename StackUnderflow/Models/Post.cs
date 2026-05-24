3f03f4030ede5d530567e4cd9604c2204435836b
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2026-05-24 14:35:45 -0500

    add answer sort

--- StackUnderflow/Models/Post.cs
+++ StackUnderflow/Models/Post.cs
@@ -1,7 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 namespace StackUnderflow.Models;
 
